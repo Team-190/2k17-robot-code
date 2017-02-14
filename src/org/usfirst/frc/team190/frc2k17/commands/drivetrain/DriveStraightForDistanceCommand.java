b2438f63369072f225124469b9af1ab46aceee80
Ryan LaPointe <rrlapointe@outlook.com>
2017-02-14 18:47:04 -0500

    Start working on heading correction while driving straight

--- src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/DriveStraightForDistanceCommand.java
+++ src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/DriveStraightForDistanceCommand.java
@@ -24,11 +24,12 @@ public class DriveStraightForDistanceCommand extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.drivetrain.enableDistanceControl(inches);
+    	Robot.drivetrain.enableTurningControl(0);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.controlDistance();
+    	Robot.drivetrain.controlTurningAndDistance();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -39,11 +40,12 @@ public class DriveStraightForDistanceCommand extends Command {
     // Called once after isFinished returns true
     protected void end() {
     	Robot.drivetrain.disableDistanceControl();
+    	Robot.drivetrain.disableTurningControl();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.drivetrain.disableDistanceControl();
+    	end();
     }
 }
