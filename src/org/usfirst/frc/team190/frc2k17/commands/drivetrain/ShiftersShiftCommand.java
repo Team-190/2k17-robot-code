4cdbae44b30046355d018b5cbbb49a1b48812824
Ryan LaPointe <rrlapointe@outlook.com>
2017-02-21 18:32:44 -0500

    Remove redundant code in ShiftersShiftCommand

--- src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/ShiftersShiftCommand.java
+++ src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/ShiftersShiftCommand.java
@@ -8,41 +8,26 @@ import org.usfirst.frc.team190.frc2k17.RobotMap;
 import org.usfirst.frc.team190.frc2k17.subsystems.drivetrain.Shifters;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
 /**
  *
  */
-public class ShiftersShiftCommand extends Command {
+public class ShiftersShiftCommand extends TimedCommand {
 
 	private final Shifters.Gear gear;
-	private boolean finished = false;
-	private Timer timer;
-	private TimerTask task;
 	
     public ShiftersShiftCommand(Shifters.Gear gear) {
+    	super(RobotMap.getInstance().SHIFT_PAUSE.get());
         requires(Robot.shifters);
         requires(Robot.drivetrain);
         this.gear = gear;
-        timer = new Timer();
-        task = new TimerTask() {
-
-			@Override
-			public void run() {
-				finished = true;
-			}
-        	
-        };
-        timer.schedule(task, RobotMap.getInstance().SHIFT_PAUSE.get());
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.drivetrain.tankDrive(0, 0);
     	Robot.shifters.shift(gear);
-    }
-
-    protected boolean isFinished() {
-        return finished;
     }
 
 }
