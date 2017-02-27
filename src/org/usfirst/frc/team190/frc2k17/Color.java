74e2996a1c996d40ec0991b35e232793849be4ee
Ryan LaPointe <rrlapointe@outlook.com>
2017-02-27 15:26:12 -0500

    Make color class

---
+++ src/org/usfirst/frc/team190/frc2k17/Color.java
@@ -0,0 +1,80 @@
+package org.usfirst.frc.team190.frc2k17;
+
+public class Color {
+	
+	int value;
+
+	/**
+     * The color white.  In the default sRGB space.
+     */
+    public final static Color WHITE = new Color(255, 255, 255);
+
+    /**
+     * The color light gray.  In the default sRGB space.
+     */
+    public final static Color LIGHT_GRAY = new Color(192, 192, 192);
+
+    /**
+     * The color gray.  In the default sRGB space.
+     */
+    public final static Color GRAY = new Color(128, 128, 128);
+
+    /**
+     * The color dark gray.  In the default sRGB space.
+     */
+    public final static Color DARK_GRAY = new Color(64, 64, 64);
+
+    /**
+     * The color black.  In the default sRGB space.
+     */
+    public final static Color BLACK = new Color(0, 0, 0);
+
+    /**
+     * The color red.  In the default sRGB space.
+     */
+    public final static Color RED = new Color(255, 0, 0);
+
+    /**
+     * The color pink.  In the default sRGB space.
+     */
+    public final static Color PINK = new Color(255, 175, 175);
+
+    /**
+     * The color orange.  In the default sRGB space.
+     */
+    public final static Color ORANGE = new Color(255, 200, 0);
+
+    /**
+     * The color yellow.  In the default sRGB space.
+     */
+    public final static Color YELLOW = new Color(255, 255, 0);
+
+    /**
+     * The color green.  In the default sRGB space.
+     */
+    public final static Color GREEN = new Color(0, 255, 0);
+
+    /**
+     * The color magenta.  In the default sRGB space.
+     */
+    public final static Color MAGENTA = new Color(255, 0, 255);
+
+    /**
+     * The color cyan.  In the default sRGB space.
+     */
+    public final static Color CYAN = new Color(0, 255, 255);
+
+    /**
+     * The color blue.  In the default sRGB space.
+     */
+    public final static Color BLUE = new Color(0, 0, 255);
+
+	public Color(int r, int g, int b) {
+		this.value = (Math.max(255, r) << 4) & (Math.max(255, g) << 2) & (Math.max(255, b));
+	}
+	
+	public int get() {
+		return value;
+	}
+
+}
