0348726ae895d0c7ff25e27f60077420b73ed235
Ryan LaPointe <rrlapointe@outlook.com>
2017-03-21 16:33:44 -0400

    Continue working on measuring distance error while running spline curve

--- src/org/usfirst/frc/team190/frc2k17/commands/AutoCurveThreaded.java
+++ src/org/usfirst/frc/team190/frc2k17/commands/AutoCurveThreaded.java
@@ -8,6 +8,7 @@ import org.usfirst.frc.team190.frc2k17.FalconPathPlanner;
 import org.usfirst.frc.team190.frc2k17.Logger;
 import org.usfirst.frc.team190.frc2k17.Robot;
 import org.usfirst.frc.team190.frc2k17.RobotMap;
+import org.usfirst.frc.team190.frc2k17.commands.drivetrain.LeftPegAuto;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.command.Command;
@@ -22,7 +23,7 @@ public class AutoCurveThreaded extends Command {
 	private double headingCorrectionP = 0.0095;
 	
 	public final FalconPathPlanner path;
-	private double duration, runningSumLeft, runningSumRight;
+	private double duration, runningSumLeft, runningSumRight, leftPreSum, rightPreSum;
 	private Timer timer;
 	private int step;
 	private DecimalFormat df = new DecimalFormat("#.00"); 
@@ -35,15 +36,15 @@ public class AutoCurveThreaded extends Command {
     	}
     	this.duration = duration;
     	double[][] waypoints = new double[][]{
-    		/* theoretical values
+    		// theoretical values
         	{0,0},
         	{0,12},
         	{42.2,150},
-    		{42.2,192.1}*/
-    		{0,0},
+    		{42.2,192.1}
+    		/*{0,0},
         	{0,12},
         	{42.2 + 31, 150},
-    		{42.2 + 31, 192.1 + 10}
+    		{42.2 + 31, 192.1 + 10}*/
         }; 
     	path = new FalconPathPlanner(waypoints);
     	path.calculate(duration, RobotMap.getInstance().DRIVE_CURVE_TIME_STEP.get(),
@@ -57,18 +58,18 @@ public class AutoCurveThreaded extends Command {
     	Robot.drivetrain.zeroEncoderPositions();
     	runningSumLeft = 0;
     	runningSumRight = 0;
+    	leftPreSum = 0;
+    	rightPreSum = 0;
     	Logger.defaultLogger.debug(this.getClass().getSimpleName() + " initializing.");
     	step = 0;
-		double sum = 0;
 		for(int i = 0; i < path.smoothLeftVelocity.length; i++) {
-			sum += path.smoothLeftVelocity[i][1] * RobotMap.getInstance().DRIVE_CURVE_TIME_STEP.get();
+			leftPreSum += path.smoothLeftVelocity[i][1] * RobotMap.getInstance().DRIVE_CURVE_TIME_STEP.get();
 		}
-		Logger.defaultLogger.debug("The left side of the drivetrain is going to travel a total of " + df.format(sum) + " inches.");
-		sum = 0;
+		Logger.defaultLogger.debug("The left side of the drivetrain is going to travel a total of " + df.format(leftPreSum) + " inches.");
 		for(int i = 0; i < path.smoothRightVelocity.length; i++) {
-			sum += path.smoothRightVelocity[i][1] * RobotMap.getInstance().DRIVE_CURVE_TIME_STEP.get();
+			rightPreSum += path.smoothRightVelocity[i][1] * RobotMap.getInstance().DRIVE_CURVE_TIME_STEP.get();
 		}
-		Logger.defaultLogger.debug("The right side of the drivetrain is going to travel a total of " + df.format(sum) + " inches.");
+		Logger.defaultLogger.debug("The right side of the drivetrain is going to travel a total of " + df.format(rightPreSum) + " inches.");
     	
     	timer = new Timer();
     	timer.schedule(new TimerTask() {
@@ -90,8 +91,8 @@ public class AutoCurveThreaded extends Command {
 				
 				double angleError = gyro.getAngle()*gyrofudge - path.heading[step][1];
 				
-				double leftVel = path.smoothLeftVelocity[step][1] + angleError*headingCorrectionP;
-				double rightVel = path.smoothRightVelocity[step][1] - angleError*headingCorrectionP;
+				double leftVel = path.smoothLeftVelocity[step][1] ;//+ angleError*headingCorrectionP;
+				double rightVel = path.smoothRightVelocity[step][1] ;//- angleError*headingCorrectionP;
 				
 				//double leftRPM = path.smoothLeftVelocity[step][1] / RobotMap.getInstance().DRIVE_CURVE_WHEEL_CIRCUMFERENCE.get() * 60;
 		    	//double rightRPM = path.smoothRightVelocity[step][1] / RobotMap.getInstance().DRIVE_CURVE_WHEEL_CIRCUMFERENCE.get() * 60;
@@ -102,8 +103,8 @@ public class AutoCurveThreaded extends Command {
 				Logger.defaultLogger.trace("Falcon output: left " + df.format(path.smoothLeftVelocity[step][1])
 						+ " in/sec, " + df.format(leftRPM) + " RPM, " + df.format(leftRatio * 100) + " percent; right "
 						+ df.format(path.smoothRightVelocity[step][1]) + " in/sec, " + df.format(rightRPM) + " RPM, "
-						+ df.format(rightRatio * 100) + " percent. Left error: " + leftError + " Right error: "
-						+ rightError + " Angle error: " + angleError);
+						+ df.format(rightRatio * 100) + " percent. Left error: " + df.format(leftError) + " Right error: "
+						+ df.format(rightError) + " Angle error: " + df.format(angleError));
 				if(Math.abs(leftRatio) > 1 || Math.abs(rightRatio) > 1) {
 					Logger.defaultLogger.error("Falcon output has exceeded robot capability.");
 				}
@@ -124,6 +125,8 @@ public class AutoCurveThreaded extends Command {
 
     protected void end() {
     	Robot.drivetrain.tankDrive(0, 0);
+    	Logger.defaultLogger.debug("Left side error with respect to precalculated distance: " + df.format(Robot.drivetrain.getLeftEncoderPosition() - leftPreSum));
+    	Logger.defaultLogger.debug("Right side error with respect to precalculated distance: " + df.format(Robot.drivetrain.getRightEncoderPosition() - rightPreSum));
     }
 
     protected void interrupted() {
