9655c11923b1417ef5eed6d2e0875ad83b54b4a6
DriverStation <driverstation@example.com>
2017-04-01 10:18:30 -0400

    Fix shooting plus gear auto

--- src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/AutoShootingPlusGearCommandGroup.java
+++ src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/AutoShootingPlusGearCommandGroup.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team190.frc2k17.commands.drivetrain;
 
 import org.usfirst.frc.team190.frc2k17.commands.shooter.FeederFeedCommand;
+import org.usfirst.frc.team190.frc2k17.commands.shooter.ShooterSpinCommand;
 import org.usfirst.frc.team190.frc2k17.commands.shooter.ShooterSpinToSpeedCommand;
 import org.usfirst.frc.team190.frc2k17.commands.shooter.ShooterStopCommand;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
 /**
  *
@@ -30,14 +32,15 @@ public class AutoShootingPlusGearCommandGroup extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	addSequential(new ShooterSpinToSpeedCommand());
+    	addParallel(new ShooterSpinCommand());
+    	addSequential(new TimedCommand(0.5));
     	addSequential(new FeederFeedCommand(2));
     	addSequential(new ShooterStopCommand());
     	if(DriverStation.getInstance().getAlliance()  == Alliance.Blue) {
-    		addSequential(new TankDriveValueCommand(0, 0.5, 1));
+    		addSequential(new TankDriveValueCommand(-0.5, 0, 1));
     		addSequential(new LeftPegAuto(true));
     	} else {
-    		addSequential(new TankDriveValueCommand(0.5, 0, 1));
+    		addSequential(new TankDriveValueCommand(0, -0.5, 1));
     		addSequential(new RightPegAuto(true));
     	}
     	   	
