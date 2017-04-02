3e84b70441f4965b8b19ed3276c39eed963299ea
DriverStation <driverstation@example.com>
2017-04-02 10:59:31 -0400

    Further fix vision side peg auto

--- src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/LeftPegAutoDriveAcrossField.java
+++ src/org/usfirst/frc/team190/frc2k17/commands/drivetrain/LeftPegAutoDriveAcrossField.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team190.frc2k17.commands.drivetrain;
 
 import org.usfirst.frc.team190.frc2k17.commands.ChangeGearKickAfterwardsCommand;
 import org.usfirst.frc.team190.frc2k17.commands.cameraLight.GearCameraLightOffCommand;
+import org.usfirst.frc.team190.frc2k17.commands.gearplacer.SetAutoKickEnabledCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -12,6 +13,7 @@ public class LeftPegAutoDriveAcrossField extends CommandGroup {
 
     public LeftPegAutoDriveAcrossField() {
     	addSequential(new ChangeGearKickAfterwardsCommand(null));
+    	addSequential(new SetAutoKickEnabledCommand(false));
     	addSequential(new GearCameraLightOffCommand());
     	addSequential(new TurnToDegreesCommand(-60));
     	addSequential(new DriveStraightForDistanceCommand(197.65));
